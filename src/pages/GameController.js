1e6462564338e5824a3202d0509ad6235c90c79e
Axonn7 <njutn.metar@gmail.com>
2021-12-28 16:12:25 +0100

    Add design, switch to touch events

--- src/pages/GameController.js
+++ src/pages/GameController.js
@@ -1,25 +1,103 @@
-import React from 'react';
-import {Button} from "@mui/material";
-import {useParams} from "react-router-dom";
+import React, {useEffect, useState} from 'react';
+import {Alert, AppBar, Button, Snackbar, Toolbar, Typography} from "@mui/material";
+import {useNavigate, useParams} from "react-router-dom";
+import {makeStyles} from "@material-ui/styles";
+import appBarLogo from "../pictures/appbarimg.png";
+
+const useStyles = makeStyles({
+    root: {
+        display: "flex",
+        marginBottom: 100,
+        flexDirection: "row",
+        width: "100%",
+        justifyContent: "center",
+        backgroundColor: "grey",
+    },
+    container: {
+        display: "flex",
+        flexDirection: "row",
+        width: "100%",
+        height: "100vh",
+        backgroundColor: "#252525",
+        justifyContent: "center",
+        alignItems: "center",
+        "& .MuiButton-root": {
+            height: "14vw",
+            width: "14vw",
+            color: "#000000",
+            marginTop: 20,
+            backgroundColor: "#ddcb7f",
+            fontWeight: "bold",
+            fontSize: 50,
+            borderRadius: 100,
+            marginLeft: 30,
+            marginRight: 30,
+        },
+        "& .MuiToolbar-root": {
+            minHeight:60,
+            height: "5vh",
+        }
+    },
+    appBar: {
+        backgroundImage: "linear-gradient(to right, black, #333333)",
+        background: "#000000",
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    gamepadContainer: {
+        width: "100%",
+        position: "fixed",
+        bottom: 25,
+    },
+    topRow: {
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
+    },
+    bottomRow: {
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
+    },
+    img: {
+        marginTop:10,
+    },
+    toast:{
+      marginTop: "5vh",
+    },
+});
 
 export default function GameController() {
+    const navigate = useNavigate();
+    const classes = useStyles();
+    const [alert, setAlert] = useState(false);
     let {gameId} = useParams();
     console.log(gameId);
     let player = 0;
     let ws = new WebSocket('wss://rainy-carpal-leptoceratops.glitch.me');
     ws.binaryType = "arraybuffer"
 
+    useEffect(() => {
+        ws.onopen = (event) => {
+            console.log("Connection is open");
+            let data = [1, gameId.charCodeAt(0), gameId.charCodeAt(1), gameId.charCodeAt(2), gameId.charCodeAt(3)];
+            let viewByteArray = new Uint8Array(data);
+            console.log(viewByteArray);
+            ws.send(viewByteArray);
 
-    ws.onopen = (event) => {
-        console.log(event.data)
-        console.log("Connection is open");
-        let data = [1, gameId.charCodeAt(0), gameId.charCodeAt(1), gameId.charCodeAt(2), gameId.charCodeAt(3)];
-        let viewByteArray = new Uint8Array(data);
-        console.log(viewByteArray);
-        ws.send(viewByteArray);
+            console.log("request to join the game sent, data: " + viewByteArray);
+        };
+    }, [])
 
-        console.log("request to join the game sent, data: " + viewByteArray);
-    };
+    useEffect(() => {
+        if(player === 0) {
+            setAlert(true);
+        } else {
+            setAlert(false);
+        }
+    }, [player])
 
     ws.onmessage = (message) => {
         if (message.data[1] !== 0) {
@@ -46,21 +124,65 @@ export default function GameController() {
 
     return (
         <>
-            <Button onMouseDown={() => handleEvent(player, 0, 1, gameId, ws)}
-                    onMouseUp={() => handleEvent(player, 0, 0, gameId, ws)}>Nazad</Button>
+            <meta name="viewport" content="user-scalable=no"/>
+            <div className={classes.container}>
+                <Snackbar
+                    className={classes.toast}
+                    anchorOrigin={{vertical: "top", horizontal: "right"}}
+                    open={alert}
+                    autoHideDuration={5000}
+                    onClose={() => setAlert(false)}
+                >
+                    <Alert variant="filled" severity="error">
+                        Neispravan kod, tražena igra ne postoji.
+                    </Alert>
+                </Snackbar>
+                <AppBar position="fixed">
+                    <Toolbar className={classes.appBar}>
+                        <Typography variant="h6" color="inherit" component="div">
+                            <img
+                                className={classes.img}
+                                src={appBarLogo}
+                                alt={"Kahoochines"}
+                                onClick={home}
+                            />
+                        </Typography>
+                    </Toolbar>
+                </AppBar>
+                <div className={classes.gamepadContainer}>
+                    <div className={classes.topRow}>
+                        <Button onTouchStart={() => handleEvent(player, 1, 1, gameId, ws)}
+                                onTouchEnd={() => handleEvent(player, 1, 0, gameId, ws)}>↑</Button>
 
-            <Button onMouseDown={() => handleEvent(player, 1, 1, gameId, ws)}
-                    onMouseUp={() => handleEvent(player, 1, 0, gameId, ws)}>Naprijed</Button>
+                        <Button onTouchStart={() => handleEvent(player, 3, 1, gameId, ws)}
+                                onTouchEnd={() => handleEvent(player, 3, 0, gameId, ws)}>→</Button>
+                    </div>
+                    <div className={classes.bottomRow}>
+                        <div>
+                            <Button onTouchStart={() => handleEvent(player, 4, 1, gameId, ws)}
+                                    onTouchEnd={() => handleEvent(player, 4, 0, gameId, ws)}>✴</Button>
+                            <Button onTouchStart={() => handleEvent(player, 2, 1, gameId, ws)}
+                                    onTouchEnd={() => handleEvent(player, 2, 0, gameId, ws)}>←</Button>
+                        </div>
 
-            <Button onMouseDown={() => handleEvent(player, 2, 1, gameId, ws)}
-                    onMouseUp={() => handleEvent(player, 2, 0, gameId, ws)}>Lijevo</Button>
 
-            <Button onMouseDown={() => handleEvent(player, 3, 1, gameId, ws)}
-                    onMouseUp={() => handleEvent(player, 3, 0, gameId, ws)}>Desno</Button>
+                        <div>
+                            <Button onTouchStart={() => handleEvent(player, 0, 1, gameId, ws)}
+                                    onTouchEnd={() => handleEvent(player, 0, 0, gameId, ws)}>↓</Button>
 
-            <Button onMouseDown={() => handleEvent(player, 4, 1, gameId, ws)}
-                    onMouseUp={() => handleEvent(player, 4, 0, gameId, ws)}>Pucaj</Button>
+                            <Button onTouchStart={() => handleEvent(player, 4, 1, gameId, ws)}
+                                    onTouchEnd={() => handleEvent(player, 4, 0, gameId, ws)}>✴</Button>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
 
         </>
     );
+
+
+    function home() {
+        navigate("/");
+    }
 }
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,21 +1,117 @@
 import React, {useState} from 'react';
-import {Button, TextField} from "@mui/material";
+import {AppBar, Button, TextField, Toolbar, Typography} from "@mui/material";
+import {makeStyles} from "@material-ui/styles";
 import {useNavigate} from "react-router-dom";
+import appBarLogo from "../../src/pictures/appbarimg.png"
 
+const useStyles = makeStyles({
+    root: {
+        display: "flex",
+        flexDirection: "row",
+        marginBottom: 100,
+        width: "100%",
+        justifyContent: "center",
+        backgroundColor:"grey",
+    },
+    container: {
+        display:"flex",
+        flexDirection:"row",
+        width:"100%",
+        height:"100vh",
+        backgroundColor:"#252525",
+        justifyContent:"center",
+        alignItems:"center",
+    },
+    appBar: {
+        backgroundImage: "linear-gradient(to right, black, #333333)",
+        background: "#000000",
+        display: "flex",
+        flexDirection: "row",
+        justifyContent:"center",
+        alignItems: "center",
+        height:"97px",
+    },
+    form: {
+        display: "flex",
+        flexDirection: "column",
+        paddingTop: 110,
+        justifyContent: "center",
+        "& .MuiFormLabel-root": {
+            color: "#C7AD66",
+        },
+        "& .MuiInputLabel-root.Mui-focused": {
+            color: "#C7AD66",
+        },
+        "& .MuiOutlinedInput-input":{
+            color: "#ddcb7f",
+            textAlign:"center",
+            textTransform: "uppercase",
+        },
+        "& .MuiButton-root": {
+            color: "#000000",
+            marginTop:20,
+            backgroundColor:"#ddcb7f",
+            fontWeight:"bold",
+        },
+        '& label.Mui-focused': {
+            color: 'white',
+        },
+        '& .MuiOutlinedInput-root': {
+            '& fieldset': {
+                borderColor: "#C7AD66",
+            },
+            '&:hover fieldset': {
+                borderColor: "#C7AD66",
+            },
+            '&.Mui-focused fieldset': {
+                borderColor: "#C7AD66",
+            },
+        },
+    },
+    inputField: {
+        display: "flex",
+        width: 200,
+        justifyContent: "center",
+        backgroundColor:"#404040",
+        borderRadius:"10px",
+        borderColor:"black",
+    },
+});
 
 export default function Homepage() {
+    const classes = useStyles();
     const navigate = useNavigate();
     const [input, setInput] = useState('');
 
     return (
         <>
-            <TextField id="inputField" label="Unesite naziv igre" variant="outlined"
-                       onChange={e => setInput(e.target.value)}/>
-            <Button onClick={onClick}> Kreni </Button>
+            <div className={classes.container}>
+                <AppBar position="fixed">
+                    <Toolbar className={classes.appBar}>
+                        <Typography variant="h6" color="inherit" component="div">
+                            <img
+                                src={appBarLogo}
+                                alt={"Kahoochines"}
+                                onClick={home}
+                            />
+                        </Typography>
+                    </Toolbar>
+                </AppBar>
+                <div className={classes.form}>
+                    <TextField className={classes.inputField} id="inputField" label="Unesite kod igre"
+                               variant="outlined"
+                               onChange={e => setInput(e.target.value)}/>
+                    <Button onClick={onClick}> Kreni </Button>
+                </div>
+            </div>
         </>
     );
 
     function onClick() {
         navigate("/controller/" + input);
+    }
+
+    function home() {
+        navigate("/");
     }
 }
